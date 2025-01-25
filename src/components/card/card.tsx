2060031d65a2a35e5a11c62160288893bb3f345d
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-01-25 17:45:32 +0200

    adding a flip animation for only two cards

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -4,24 +4,21 @@ import { ICard } from "../../types/@types";
 
 interface IProps {
   card: ICard;
+  passEvent: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 const card = (props: IProps) => {
-  const [visible, setVisible] = useState(props.card.visible);
-
+  
   return (
     <div
       onClick={(e) => {
-        setVisible(!props.card.visible);
-        props.card.visible = !props.card.visible;
-        e.currentTarget.classList.toggle("flip");
+        props.passEvent(e);
       }}
       style={{
-        backgroundImage: visible
+        backgroundImage: props.card.visible
           ? `url(https://clipart.com/thumbs.php?f=/1500/batch_12/001500-0012-0000${props.card.option}_tnb.png)`
           : "none",
       }}
       className="card"
-      data-image="url(https://cdn.pixabay.com/photo/2017/10/03/20/30/book-2814026_1280.jpg)"
     >
       {props.card.id}
     </div>
--- src/screens/game.screen.tsx
+++ src/screens/game.screen.tsx
@@ -7,14 +7,28 @@ import { createGameBoard } from "../utils/game.util";
 
 const GameScreen = () => {
   const CURRENT_LEVEL = ELevels.MEDIUM;
-  
-  const [cards] = useState<ICard[]>(createGameBoard(CURRENT_LEVEL)); // I've deleted the setCards since its not used here yet.
+  const [cards] = useState<ICard[]>(createGameBoard(CURRENT_LEVEL));
+  const [visible, setVisible] = useState(false);
+  const [invokedCard, setInvokedCard] = useState<
+    React.MouseEvent<HTMLDivElement, MouseEvent>[]
+  >([]);
+
+  const handleOnClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (invokedCard.length < 2) {
+      setInvokedCard((prevInvoked) => [...prevInvoked, e]);
+      e.currentTarget.classList.add("flip");
+      setVisible(true);
+    } else {
+      console.log(invokedCard);
+    }
+  };
+  // I've deleted the setCards since its not used here yet.
   return (
     <div className="screen game-screen">
       <div className="placeholder">Status</div>
       <div className={`placeholder game Level_${CURRENT_LEVEL}`}>
         {cards.map((card, index) => (
-          <Card key={index} card={card}/>
+          <Card key={index} card={card} passEvent={handleOnClick} />
         ))}
       </div>
     </div>
