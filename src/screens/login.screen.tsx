0fd15fa42e8e5a9b0ce078a087b64099cb8f409f
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-01-26 17:07:41 +0200

    setting up the logic for the login page.

--- src/screens/login.screen.tsx
+++ src/screens/login.screen.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Button, TextField } from "@mui/material";
+import React, { useRef } from "react";
 
 const LoginScreen = () => {
-  return <div></div>;
+  const nameField = useRef<HTMLInputElement>(null);
+  const handlePlayButton = () => {
+    if (nameField.current) {
+      console.log(nameField.current.value);
+    }
+  };
+  return (
+    <div>
+      <div className="login-form">
+        <TextField
+          inputRef={nameField}
+          className="custom-mui-textfield"
+          id="outlined-basic"
+          label="Player Name"
+          variant="outlined"
+        />
+        <Button
+          onClick={handlePlayButton}
+          id="play"
+          className="custom-mui-button"
+          variant="contained"
+        >
+          Start Playing
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginScreen;
