c622d7603f8554fe86299893cc08b28b7d25e4b5
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-01-26 16:10:20 +0200

    fixing the card flip time duration (added a front and a back faces)

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -11,14 +11,17 @@ const card = (props: IProps) => {
       onClick={() => {
         props.passEvent(props.card);
       }}
-      style={{
-        backgroundImage: props.card.visible
-          ? `url(https://clipart.com/thumbs.php?f=/1500/batch_12/001500-0012-0000${props.card.option}_tnb.png)`
-          : "none",
-      }}
       className={`card ${props.card.isFlipped ? "flip hideQM" : ""}`}
     >
-      ?
+      <div className="card-front">?</div>
+      <div
+        className="card-back"
+        style={{
+          backgroundImage: props.card.visible
+            ? `url(https://clipart.com/thumbs.php?f=/1500/batch_12/001500-0012-0000${props.card.option}_tnb.png)`
+            : "none",
+        }}
+      ></div>
     </div>
   );
 };
--- src/screens/game.screen.tsx
+++ src/screens/game.screen.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./screens.css";
 import Card from "../components/card/card";
 import { ELevels, ICard } from "../types/@types";
