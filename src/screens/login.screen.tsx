73660b1b13161f036d5774e0fe0e18bc883d0dae
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-01-27 14:20:32 +0200

    buffing the style.

--- src/screens/login.screen.tsx
+++ src/screens/login.screen.tsx
@@ -1,7 +1,17 @@
-import { Button, TextField } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from "@mui/material";
 import { useContext, useRef } from "react"; // Adjust the import path as necessary
 import { authContext } from "../providers/authProvider";
 import { useNavigate } from "react-router-dom";
+import React from "react";
+import { ELevels } from "../types/@types";
 
 const LoginScreen = () => {
   const nameField = useRef<HTMLInputElement>(null);
@@ -15,6 +25,13 @@ const LoginScreen = () => {
       navigate("/game");
     }
   };
+
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
   return (
     <div>
       <div className="login-form">
@@ -24,13 +41,21 @@ const LoginScreen = () => {
           id="outlined-basic"
           label="Player Name"
           variant="outlined"
-          InputProps={{
-            style: { color: "white" }, // Text color set to white
-          }}
-          InputLabelProps={{
-            style: { color: "white" }, // Label color set to white
-          }}
         />
+        <FormControl fullWidth className="custom-mui-dropdown">
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={ELevels.EASY}>Easy</MenuItem>
+            <MenuItem value={ELevels.MEDIUM}>Medium</MenuItem>
+            <MenuItem value={ELevels.HARD}>Hard</MenuItem>
+          </Select>
+        </FormControl>
         <Button
           onClick={handlePlayButton}
           id="play"
